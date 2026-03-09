69005041e9841347fa7bccc07d7e5bafec73260a
Sushant kumar Jha <jhasushantkumar592@gmail.com>
2026-03-09 18:25:41 +0530

    Add files via upload

---
+++ babu.py
@@ -0,0 +1,141 @@
+# n=int(input("enter the number"))
+# rev=0
+# for i in range(n):
+#     rev=rev*10+n%10
+#     n=n//10
+# print ("reverse number is ",rev)
+
+
+# n=int(input("enter a number"))
+# rev=0
+# x=n
+# while(n>0):
+#     rev=rev*10+n%10
+#     n=n//10
+# if(x==rev):
+#     print("palindrome")
+
+
+
+# n=int(int(input("enter a number")))
+# rev=0
+# while n>0:
+#     rev=rev*10+n%10
+#     n=n//10
+# print("reverse of number is",rev)
+
+
+# n=int(input("enter a number"))
+# sum=0
+# x=n
+# while n>0:
+#     sum=sum+(n%10)*(n%10)*(n%10)
+#     n=n//10
+# print(sum)
+# if sum==x:
+#     print("armstrong")
+# else:
+#     print("not armstrong")
+
+
+# n=int(input("enter a number1"))
+# m=int((input "enter a number2"))
+# sum=n+m
+# print(sum)
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# sum=a+b
+# print(sum)
+# n=int(input("enter a number"))
+# rev=0
+# while n>0:
+#     rev=rev*10+n%10
+#     n=n//10
+# print("rev of number is",rev)
+
+# n=int(input("enter a number"))
+# sum=0
+# while n>0:
+#     sum=sum+(n%10)*(n%10)*(n%10)
+#     n=n//10
+# print("sum of digit is",sum)
+
+
+# class Circle:
+#     def __init__(self,radius):
+#         self.radius=radius
+#     def area(self):
+#         area=22/7*(self.radius)**2
+#         return area
+#     def perimeter(self):
+#         perimeter=2*22/7*(self.radius)
+#         return perimeter
+# c1=Circle(14)
+# print(c1.area())
+# print(c1.perimeter())
+
+# class student:
+#     def __init__(self,fullname,marks):
+#         self.name=fullname
+#         self.marks=marks
+# obj1=student("SUSHANT",99)
+# print(obj1.name,obj1.marks)
+
+# str="SUSHANT"
+# for i in range(len(str)-1,-1,-1):
+#   print(str[i],end=" ")
+
+# vowels="aeiou"
+# str="sushant"
+# count=0
+# for var in str:
+#     if var in vowels:
+#         count=count+1
+# print(count)
+
+
+# str="i love india"
+# for i in range(len(str)-1,-1,-1):
+#     print(str[i],end=" ")
+
+# str="i love india"
+# print(str[-1::-1])
+
+
+
+
+
+
+
+# str="my name is sushant kumar jha"
+# for i in range (len(str)-1,-1,-1):
+#     print(str[i],end=" ")
+
+
+# n=int(input("enter a number"))
+# rev=0
+# while n>0:
+#     rev=rev*10+n%10
+#     n=n//10
+# print("rev of no. is",rev)
+
+n=int(input("enter a number"))
+sum=0
+x=n
+while n>0:
+    sum=sum+(n%10)*(n%10)*(n%10)
+    n=n//10
+print("sum of digir is",sum)
+if x==sum:
+    print("armstrong")
+else:
+    ("not armstrong")
+
+
+
+
+    
+    
+
+      
+
